7737915e8214c97ef18e86e46dd109e2a542f4b6
Adrian Machado <amachado@uwaterloo.ca>
2023-09-08 14:14:34 -0700

    Call external SSO endpoint

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,21 +1,46 @@
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server'
+import { createRouteHandlerClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
-export const dynamic = 'force-dynamic'
+export const dynamic = "force-dynamic";
 
 export async function GET(request: Request) {
   // The `/auth/callback` route is required for the server-side auth flow implemented
   // by the Auth Helpers package. It exchanges an auth code for the user's session.
   // https://supabase.com/docs/guides/auth/auth-helpers/nextjs#managing-sign-in-with-code-exchange
-  const requestUrl = new URL(request.url)
-  const code = requestUrl.searchParams.get('code')
+  const requestUrl = new URL(request.url);
+  const code = requestUrl.searchParams.get("code");
 
   if (code) {
-    const supabase = createRouteHandlerClient({ cookies })
-    await supabase.auth.exchangeCodeForSession(code)
+    const supabase = createRouteHandlerClient({ cookies });
+    const sessionData = await supabase.auth.exchangeCodeForSession(code);
+    const { user } = sessionData?.data;
+    if (!user) {
+      return NextResponse.redirect(
+        `${requestUrl.origin}/login?error=Could not authenticate user`,
+        {
+          status: 301,
+        },
+      );
+    }
+    await fetch(
+      "http://localhost:4100/docs/zp/auth/external-sso?dev-portal-host=chocolate-leech-main-b8c57ea.d2.zuplo.dev",
+      {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+          "dev-portal-secret": "testing12345",
+        },
+        body: JSON.stringify({
+          email: user.email,
+          name: user.user_metadata?.full_name,
+          email_verified: user.confirmed_at != null,
+          sub: user.id,
+        }),
+      },
+    );
   }
 
   // URL to redirect to after sign in process completes
-  return NextResponse.redirect(requestUrl.origin)
+  return NextResponse.redirect(requestUrl.origin);
 }
--- app/auth/sign-in/route.ts
+++ app/auth/sign-in/route.ts
@@ -12,7 +12,7 @@ export async function POST(request: Request) {
   const password = String(formData.get("password"));
   const supabase = createRouteHandlerClient({ cookies });
 
-  const { error } = await supabase.auth.signInWithPassword({
+  const { data, error } = await supabase.auth.signInWithPassword({
     email,
     password,
   });
@@ -25,6 +25,33 @@ export async function POST(request: Request) {
       },
     );
   }
+
+  const { user } = data;
+  if (!user) {
+    return NextResponse.redirect(
+      `${requestUrl.origin}/login?error=Could not authenticate user`,
+      {
+        status: 301,
+      },
+    );
+  }
+
+  await fetch(
+    "http://localhost:4100/docs/zp/auth/external-sso?dev-portal-host=chocolate-leech-main-b8c57ea.d2.zuplo.dev",
+    {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+        "dev-portal-secret": "testing12345",
+      },
+      body: JSON.stringify({
+        email: user.email,
+        name: user.user_metadata?.full_name,
+        email_verified: user.confirmed_at != null,
+        sub: user.id,
+      }),
+    },
+  );
 
   // Redirect back home after sign-in, with the tokenRedirectUrl
   return NextResponse.redirect(
